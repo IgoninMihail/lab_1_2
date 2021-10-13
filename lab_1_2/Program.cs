b3efc5aa7466d8425bfa27c4de22d268c43eb2a2
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-10-13 18:36:23 +0200

    Добавьте файлы проекта.

---
+++ lab_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double y, b, a, x;
+                Console.WriteLine("Ввести (y) Прибыль:");
+                y = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ввести (b) Себестоимость:");
+                b = double.Parse(Console.ReadLine());
+                a = (b / 100) * 105;
+                x = ((y / a) / 100) * 100;
+                Console.WriteLine("Рентабельность:x={0:F2}", ((y / a) / 100) * 100);
+            }
+            catch
+            {
+                Console.WriteLine("Введите корректно прибыль и себестоимость");
+            }
+            
+        }
+    }
+}
